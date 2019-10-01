d01cca9e5f12c27be587d40dfe0d9c4513f98a8e
ptv-z79 <t18v05po@gmail.com>
2019-10-01 19:13:57 +0300

    Delete подсветка в asm.asm

--- Lua/подсветка в asm.asm
+++
@@ -1,7 +0,0 @@
-; хз что это такое, просто подсветка красивая!!! :)
-anims_name_fix_1 proc
- 	lea		eax, [esp+18h]
- 	PRINT_UINT "** Loaded animation %s", eax
- 	add     dword ptr [esp+10h], 1
- 	jmp back_from_anims_name_fix_1
- anims_name_fix_1 endp
