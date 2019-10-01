07e50a606e4c5dd48a17dafd3b1d7feb2cb5c8c6
ptv-z79 <t18v05po@gmail.com>
2019-10-01 19:14:51 +0300

    Update test.lua

--- Lua/test.lua
+++ Lua/test.lua
@@ -1,17 +1,1 @@
-```ruby
--- файл создан для просмотра подсветки на GitHub
-local a = 10
-for i=a,13 do
-  print(i)
-end
 
-{% highlight ruby %}
-def foo
-  puts 'foo'
-end
-{% endhighlight %}
-{% highlight lua %}
-def foo
-  puts 'foo'
-end
-{% endhighlight %}
